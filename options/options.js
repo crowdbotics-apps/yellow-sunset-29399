dd54c45d9a19f12b6a0b3e4a8869922eae330719
Crowdbotics <team@crowdbotics.com>
2021-08-03 14:39:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "yellow_sunset_29399Identifier",
+  url: "https://yellow_sunset_29399Identifier.botics.co",
+  api: "https://yellow_sunset_29399Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
